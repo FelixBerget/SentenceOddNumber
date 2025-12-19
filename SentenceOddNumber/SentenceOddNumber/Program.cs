1615d600e0836a47a8a04301d44eaf6b49b9968c
Kasper Haugestøl <kasper@haugestol.com>
2025-12-19 12:45:35 +0100

    working

--- SentenceOddNumber/SentenceOddNumber/Program.cs
+++ SentenceOddNumber/SentenceOddNumber/Program.cs
@@ -5,11 +5,12 @@
         static void Main(string[] args)
         {
             SentenceDivider sentencedivider = new SentenceDivider();
-            string[] sentenceString = sentencedivider.SplitSentence("John is Here");
-            Console.WriteLine(sentenceString[0]);
-            Console.WriteLine(sentenceString[1]);
-            Console.WriteLine(sentenceString[2]);
+            string[] sentenceString = sentencedivider.SplitSentence("Hey");
 
+            foreach (var strPart in sentenceString)
+            {
+                Console.WriteLine(strPart);
+            }
         }
     }
 }
--- SentenceOddNumber/SentenceOddNumber/SentenceDivider.cs
+++ SentenceOddNumber/SentenceOddNumber/SentenceDivider.cs
@@ -6,18 +6,36 @@ namespace SentenceOddNumber
 {
     internal class SentenceDivider
     {
-
         public SentenceDivider() { }
 
         public string[] SplitSentence(string sentence)
         {
-            string[] sentenceArray = new string[sentence.Length/2];
+            string[] sentenceArray;
+
+            if (sentence.Length % 2 == 0)
+            {
+                sentenceArray = new string[sentence.Length / 2];
+            }
+            else
+            {
+                sentenceArray = new string[sentence.Length / 2 + 1];
+            }
+
             for (int i = 0; i < sentence.Length; i += 2)
             {
                 string part1 = sentence[i].ToString();
+
+                if (i == sentence.Length - 1)
+                {
+                    string underscore = "_";
+                    sentenceArray[i / 2] = part1 + underscore + "";
+                    return sentenceArray;
+                }
+
                 string part2 = sentence[i + 1].ToString();
-                sentenceArray[i/2] = part1 + part2 + "";
+                sentenceArray[i / 2] = part1 + part2 + "";
             }
+
             return sentenceArray;
         }
     }
