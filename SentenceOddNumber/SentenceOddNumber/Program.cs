bf5bfbfd00c31643d5c18c1c2efc927c68a88e69
FelixBerget <felixberget@gmail.com>
2025-12-19 10:19:30 +0100

    Pushed

---
+++ SentenceOddNumber/SentenceOddNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SentenceOddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SentenceDivider sentencedivider = new SentenceDivider();
+            string[] sentenceString = sentencedivider.SplitSentence("John is Here");
+            Console.WriteLine(sentenceString[0]);
+            Console.WriteLine(sentenceString[1]);
+            Console.WriteLine(sentenceString[2]);
+
+        }
+    }
+}
---
+++ SentenceOddNumber/SentenceOddNumber/SentenceDivider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SentenceOddNumber
+{
+    internal class SentenceDivider
+    {
+
+        public SentenceDivider() { }
+
+        public string[] SplitSentence(string sentence)
+        {
+            string[] sentenceArray = new string[sentence.Length/2];
+            for (int i = 0; i < sentence.Length; i += 2)
+            {
+                string part1 = sentence[i].ToString();
+                string part2 = sentence[i + 1].ToString();
+                sentenceArray[i/2] = part1 + part2 + "";
+            }
+            return sentenceArray;
+        }
+    }
+}
